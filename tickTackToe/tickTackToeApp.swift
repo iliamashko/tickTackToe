3cb3bf8f28ed4efeb001150890d11d1c47369d76
i.mashko <iliamashko@gmail.com>
2023-03-16 11:08:55 +0300

    Initial Commit

---
+++ tickTackToe/tickTackToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tickTackToeApp.swift
+//  tickTackToe
+//
+//  Created by Илья Машко on 16.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct tickTackToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
